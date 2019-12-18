846e60b52ddf15ac786877cea1c0fba2f1cad773
Vincent <halo_0@icloud.com>
2019-12-18 19:37:37 +0800

    jvm

--- jvm/src/Main.java
+++ jvm/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Parent.str);
+    }
+
+
+}
+class Parent{
+    public static String str="hello parent";
+
+    static{
+        System.out.println("Parent static block");
+    }
+}
+class Child extends Parent{
+    public static String str="hello child";
+
+    static{
+        System.out.println("Child static block");
+    }
 }
